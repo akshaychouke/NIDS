1579ba250d3220f268fdc7139a93637b8a05c00f
akshaychouke <akshaychauke50@gmail.com>
2026-02-27 19:43:37 +0000

    Revert "Add verbose mode options and print in formatted pattern"
    This reverts commit 5da17322972c7af9b3a1f26f26154d043066be8a.

--- src/main.c
+++ src/main.c
@@ -3,41 +3,14 @@
 #include <string.h>
 
 #include "../include/capture.h"
-#include "../include/printUtils.h"
-#include <unistd.h>
-
 #define MAXPKT -1 //for infinity
-
-static void print_usage(const char *prog) {
-    printf("Usage: %s [-v] [-d device]\n", prog);
-    printf("  -v       verbose output (multi-line)\n");
-    printf("  -d DEV   capture device (default enp0s3)\n");
-}
 
 static captureContext ctx;
 
-int main(int argc, char *argv[]) {
-    int opt;
-    const char *dev = "enp0s3";
-    int verbose = 0;
-
-    while ((opt = getopt(argc, argv, "vd:h")) != -1) {
-        switch(opt) {
-            case 'v':
-                verbose = 1;
-                break;
-            case 'd':
-                dev = optarg;
-                break;
-            case 'h':
-            default:
-                print_usage(argv[0]);
-                return 0;
-        }
-    }
-
+int main() {
     printf("Starting......\n");
-    setPrintVerbose(verbose);
+
+    const char *dev = "enp0s3";
 
     if (open_device(&ctx, dev) != 0) {
         printf("Failed to open device \n");
